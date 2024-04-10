afb40b6b754ddaed046c652113dae3610cecf1ac
valivali151633 <144232966+valivali151633@users.noreply.github.com>
2024-04-10 20:00:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
 //complete this code
-class Rectangle {}
+class Rectangle {
+	constructor(width , height) {
+		width = this.width;
+		height = this.height;
+	}
+	get myiWdth(){
+		return this.width;
+	}
+	get myHeight(){
+		return this.height;
+	}
 
-class Square extends Animal {}
+	getArea(){
+		retun this.width * this.height;
+	}
+}
+
+class Square extends Rectangle {
+
+	constructor(side){
+		super(side);
+		
+	}
+	getPerimeter(side){
+		return this.side * this.side
+		
+	}
+}
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
